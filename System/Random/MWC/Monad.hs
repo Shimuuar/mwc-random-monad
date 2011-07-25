adb24d87c2ad2ecfe92a85f1b54b5752aec65c90
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-07-26 00:45:10 +0300

    Join random genereators

--- System/Random/MWC/Monad.hs
+++ System/Random/MWC/Monad.hs
@@ -17,6 +17,9 @@ module System.Random.MWC.Monad ( -- * Random monad
                                , uniformR
                                , standard
                                , normal
+                                 -- * Combining monads 
+                               , equiprobable
+                               , choices
                                  -- * Seed management
                                , save
                                ) where
@@ -29,7 +32,9 @@ import Control.Monad.Primitive (PrimMonad, PrimState)
 import qualified Data.Vector         as V
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Generic as G
+import Data.Ord
 import Data.Word           (Word32)
+import Debug.Trace
 
 import qualified System.Random.MWC as MWC
 import           System.Random.MWC   (Gen,Variate,Seed)
@@ -128,6 +133,56 @@ normal :: PrimMonad m =>
        -> Rand m Double
 normal m s = (+ m) . (* s) <$> standard
 {-# INLINE standard #-}
+
+----------------------------------------------------------------
+
+-- FIXME: bug #4101
+delta :: Double
+delta = 2**(-53)
+{-# INLINE delta #-}
+
+-- | Randomly select from list of equiprobable random sources. List must be non-empty
+equiprobable :: PrimMonad m => [Rand m a] -> Rand m a
+equiprobable [] = error "System.Random.MWC.Monad.equiprobable: list must be nonempty"
+equiprobable xs = worker (V.fromList xs)
+  where
+    worker v = do
+      p <- uniform 
+      v V.! (truncate $ (p - delta) * fromIntegral (V.length v))
+      
+-- | Randomly select from list of weighted random sources. List must
+-- contain sources with positive weight. Elements with non-positive
+-- weight are discarded
+choices :: PrimMonad m => [(Double,Rand m a)] -> Rand m a
+choices xs
+  | null xs'  = error "System.Random.MWC.Monad.choices: list must contain at least one nonnegative weight"
+  | otherwise = traceShow (ps, V.length vect) 
+                $ worker vect ps
+  where
+    xs'  = filter ((>0) . fst) xs
+    vect = V.fromList (map snd xs')
+    ps   = U.init . U.scanl' (+) 0 $ U.map (/ U.sum q) q where q = U.fromList (map fst xs')
+    worker vect probs = do
+      p <- uniform
+      let i = binary probs p
+      vect V.! (i - 1)
+
+binary :: (Ord a, U.Unbox a) => U.Vector a -> a -> Int
+binary v x = binaryRange v x 0 (U.length v)
+{-# INLINE binary #-}
+
+binaryRange :: (Ord a, U.Unbox a) => U.Vector a -> a -> Int -> Int -> Int
+binaryRange v x = loop 
+  where
+    loop i j | i >= j    = j
+             | otherwise = case compare (U.unsafeIndex v k) x of
+                             LT -> loop (k+1) j
+                             EQ -> k  
+                             GT -> loop i k
+             where k = (i + j) `div` 2
+{-# INLINE binaryRange #-}
+
+
 
 ----------------------------------------------------------------
 
